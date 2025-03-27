6b4bea04a20ad9f769f96a166b5b3fac2285b903
ldravidu-wsl <ldravidu@gmail.com>
2025-03-27 10:40:31 +0530

    Initialize web-lms-backend project structure with Maven and Spring Boot

---
+++ web-lms-backend/src/main/java/com/ldravidu/web_lms_backend/WebLmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ldravidu.web_lms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLmsBackendApplication.class, args);
+	}
+
+}
---
+++ web-lms-backend/src/test/java/com/ldravidu/web_lms_backend/WebLmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldravidu.web_lms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
